97a5e3f1e6c779178a40867ea57d327cbf836eae
Akash srivastava <169164351+Akash1269272@users.noreply.github.com>
2024-07-02 07:49:42 +0530

    Add files via upload

--- Editor.js
+++ Editor.js
@@ -4,11 +4,9 @@ import 'codemirror/theme/material.css'
 import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
-
 import { Controlled as ControlledEditor} from 'react-codemirror2'
-import  {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faCompressAlt,faExpandAlt} from '@fortawesome/free-solid-svg-icons'
-
 
 export default function Editor(props) {
     const{
@@ -17,7 +15,7 @@ export default function Editor(props) {
         value,
         onChange
     }=props
-    const [open,setOpen] = useState(true)//icon
+    const [open,setOpen] = useState(true)
 
   function handleChange(editor,data,value){
     onChange(value)
@@ -29,8 +27,7 @@ export default function Editor(props) {
             {displayName}
             <button 
             onClick={()=>setOpen(prevOpen => !prevOpen)}
-            > <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt}/>
-            </button> 
+            > <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt}/>   </button>
         </div>
         <ControlledEditor 
           onBeforeChange={handleChange}
